fd61e7f133c3a508a4e7c43d5854b27f7352a3bd
stanleynapenas <stan.napenas@gmail.com>
2020-05-17 21:20:13 +0100

    The message app is done. Next add Beautiful Tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,13 +17,22 @@ $(window).on({
 })
 
 
+// -- FUNZIONI --
 
 // Funzione che mi manda un messaggio
 function sendMsg (){
+
+    // Salvo Data-Ref di questo bottone (Che servirà per collegare gli altri elementi)
+    var dataRefContact = $(this).attr("data-ref");
+    console.log("Questo è dataRefContact: ", dataRefContact);
+    
+
+    var parent = this;
+
+    // Mi salva l'ora
     var d = new Date;
     var h = addZero(d.getHours());
     var m = addZero(d.getMinutes());
-    console.log(h + ":" + m);
 
     function addZero(i) {
         if (i < 10) {
@@ -32,8 +41,12 @@ function sendMsg (){
         return i;
     }
 
+
+    
+
+
     // Salviamo il valore dell'input / testo
-    var msg = $(".msgTypeText").val();
+    var msg = $(".msgTypeText[data-ref=" + dataRefContact +"]").val();
     console.log(msg);
 
     // Clono il mio template / div da riutilizzare
@@ -42,55 +55,64 @@ function sendMsg (){
     // Modifica questa copia di "msgSent" aggiungendogli il testo del messaggio al paragrafo
     elementmsg.find(".msgSentText").text(msg);
     elementmsg.find(".msgTime").text(h + ":" + m);
-    console.log(h + ":" + m);
 
     // Appendiamo una copa con testo valorizzato del div "msgSent"
-    $(".msgWindowOutput").prepend(elementmsg);
+    $(".msgWindowOutput[data-ref=" + dataRefContact +"]").prepend(elementmsg);
 
     // ripuliamo il contenuto dell'input
-    $(".msgTypeText").val("");
+    $(".msgTypeText[data-ref=" + dataRefContact +"]").val("");
 
     setTimeout(sendMsgRobot, 1500);
+    function sendMsgRobot (){
+    
+        // Salvo Data-Ref di questo bottone (Che servirà per collegare gli altri elementi)
+        var dataRefContactRobot = $(parent).attr("data-ref");
+        console.log("Questo è dataRefContactRobot: ", dataRefContactRobot);
+    
+        var d = new Date;
+        var h = addZero(d.getHours());
+        var m = addZero(d.getMinutes());
+        console.log(h + ":" + m);
+    
+        function addZero(f) {
+            if (f < 10) {
+                f = "0" + f;
+            }
+            return f;
+        }
+    
+    
+    
+        // Salviamo il valore dell'input / testo
+        robotmsg = "Ciao Stan";
+        console.log(robotmsg);
+    
+        // Clono il mio template / div da riutilizzare
+        var elementmsgRobot = $("#template .msgRobot").clone();
+    
+        // Modifica questa copia di "msgSent" aggiungendogli il testo del messaggio al paragrafo
+        elementmsgRobot.find(".msgSentText.robotText").text(robotmsg);
+        elementmsgRobot.find(".msgTimeRobot").text(h + ":" + m);
+        console.log(h + ":" + m);
+    
+    
+    
+        // Appendiamo una copa con testo valorizzato del div "msgSent"
+        $(".msgWindowOutput[data-ref=" + dataRefContactRobot +"]").prepend(elementmsgRobot);
+    
+    }
 }
 
 // Funzione che il robot manda messaggio
-function sendMsgRobot (){
-    var d = new Date;
-    var h = addZero(d.getHours());
-    var m = addZero(d.getMinutes());
-    console.log(h + ":" + m);
 
-    function addZero(f) {
-        if (f < 10) {
-            f = "0" + f;
-        }
-        return f;
-    }
-
-    // Salviamo il valore dell'input / testo
-    robotmsg = "Ciao Stan";
-    console.log(robotmsg);
-
-    // Clono il mio template / div da riutilizzare
-    var elementmsgRobot = $("#template .msgRobot").clone();
-
-    // Modifica questa copia di "msgSent" aggiungendogli il testo del messaggio al paragrafo
-    elementmsgRobot.find(".msgSentText.robotText").text(robotmsg);
-    elementmsgRobot.find(".msgTimeRobot").text(h + ":" + m);
-    console.log(h + ":" + m);
-
-    // Appendiamo una copa con testo valorizzato del div "msgSent"
-    $(".msgWindowOutput").prepend(elementmsgRobot);
-
-}
-
+// Funzione di ricerca
 function search() {
     // Declare variables
     var input, filter, contactContainer, contact, name, j, txtValue;
     input = document.getElementById('search');
     filter = input.value.toUpperCase();
-    contactContainer = document.getElementsByClassName('contactContainer');
-    contact = document.getElementsByClassName('contact');
+    contactContainer = document.getElementsByClassName('contactContainer'); // Macro contenitore che conterrà i risultati
+    contact = document.getElementsByClassName('contact'); // Contenitore che contiene tutti i dati del contatto (foto, nome, testo, tempo)
   
     // Loop through all list items, and hide those who don't match the search query
     for (j = 0; j < contact.length; j++) {
@@ -120,3 +142,22 @@ function search() {
 // Array di oggetti 
 // Questi oggetti contengono nome, testo, foto, tempo
 // For each > Append
+
+
+
+
+
+
+$(".contact").click(function (){
+    var clickedcontact = $(this).attr("data-ref");
+    console.log(clickedcontact);
+    
+    $(".msgWindowOutput").removeClass("activeflex");
+    $(".msgTypeText").removeClass("active");
+    $(".msgSendBtn").removeClass("activeflex");
+
+    
+    $(".msgWindowOutput[data-ref=" + clickedcontact +"]").addClass("activeflex");
+    $(".msgTypeText[data-ref=" + clickedcontact +"]").addClass("active");
+    $(".msgSendBtn[data-ref=" + clickedcontact +"]").addClass("activeflex");
+})
